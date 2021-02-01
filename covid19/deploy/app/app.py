7e3a79122763575ec526af473cbcc7c946780b61
Alice Grosch <a.grosch@fz-juelich.de>
2021-02-01 10:40:39 +0100

    Change initial map columns

--- covid19/deploy/app/app.py
+++ covid19/deploy/app/app.py
@@ -147,12 +147,12 @@ n_people_df = pd.DataFrame(data={'n_people': n_people_array})
 # Initial maps.
 init_mapfig_bstim = create_map_figure(
     counties_geojson, counties_metadf, mapcsv_path,
-    n_people=n_people_df, column='newInf100k',
+    n_people=n_people_df, column='7DayInf100k',
     seven_days=True, incidence_values=True,
     width=500, height=450)  # size is important to ensure, that figure is created _now_ and not on resize-event
 init_mapfig_rki = create_map_figure(
     counties_geojson, counties_metadf, mapcsv_path, 
-    n_people=n_people_df, column='newInf100k_RKI',
+    n_people=n_people_df, column='7DayInf100k_RKI',
     seven_days=True, incidence_values=True,
     width=500, height=450)  # size is important to ensure, that figure is created _now_ and not on resize-event
 
